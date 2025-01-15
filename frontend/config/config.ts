78083120abd78c1fdc76bff292dca58fb973c057
SwTzu <benjamin.moralesc@alumnos.uv.cl>
2025-01-15 11:38:07 -0300

    docker compose

--- frontend/config/config.ts
+++ frontend/config/config.ts
@@ -1,4 +1,1 @@
-import dotenv from 'dotenv';
-
-dotenv.config({ path: '/app/env/.env.local' });
-export const backendUrl = `${process.env.NEXT_PUBLIC_HOST}:${process.env.NEXT_PUBLIC_PORT}/api`;
+export const backendUrl = `http://localhost:80/api`;
