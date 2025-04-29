0b6561a40f5706a3e45162c7083385ae2b70a353
MrDyslexia <mhhernand@gmail.com>
2025-04-29 19:53:39 -0400

    a

--- frontend/types/interfaces.tsx
+++ frontend/types/interfaces.tsx
@@ -105,21 +105,3 @@ export interface notaCoo {
   title: string;
   content: string;
 }
-type Practica = {
-  id: number;
-  nombreEstudiante: string;
-  rutEstudiante: string;
-  razonSocial: string;
-  empresa: string;
-  fase: number;
-  estado: string;
-  fechaSolicitud: string;
-  fechaInicio: string;
-  fechaTermino: string;
-  comentarios: string[];
-  notasCOO: { id: number; title: string; content: string }[] | null;
-  correoSupervisor: string;
-  tareas: Task[];
-  informe: boolean | null;
-  memoria: boolean | null;
-};
